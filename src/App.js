037d8719d604e036f82e5a71275aaa65b48ff11b
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-02-24 12:48:37 +0530

    Added React Router Dom to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from './components/Navbar';
 import DashboardPage from './pages/DashboardPage';
+import ApptitudePage from './pages/ApptitudePage';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"; 
 import './App.css';
 
 function App() {
   return (
     <div >
       <Navbar />
-      <DashboardPage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<DashboardPage />}/>
+          <Route path='/apptitude' element={<ApptitudePage />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
