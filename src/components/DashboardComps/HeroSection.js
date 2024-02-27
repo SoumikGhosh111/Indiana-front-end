99e4d79555b49589d17e50d18dfd163170c5c1b1
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-02-27 23:19:40 +0530

    added animations

--- src/components/DashboardComps/HeroSection.js
+++ src/components/DashboardComps/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from "framer-motion";
 import "./styles.css";
 
 function HeroSection() {
@@ -10,9 +11,23 @@ function HeroSection() {
         <div className='hero-section-wrapper'>
             <div className='hero-section-div'>
                 <div className='hero-section-hero-cont'>
-                    <h1>Get ready for your dream job</h1>
-                    <p>Practice with thousands of questions for Aptitude, Reasoning, and GS. Start now for free</p>
-                    <button onClick={(e) => handleStartPracticing(e)}>Start Practicing</button>
+                    <motion.h1
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17, duration: 0.25, delay: 0.5 }}
+                    >Get ready for your dream job</motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17, duration: 0.25, delay: 1 }}
+                    >Practice with thousands of questions for Aptitude, Reasoning, and GS. Start now for free</motion.p>
+                    <motion.button onClick={(e) => handleStartPracticing(e)}
+                          initial={{ opacity: 0, y: 30 }}
+                          animate={{ opacity: 1, y: 0 }}
+                        whileHover={{scale: 1.07}} 
+                        whileTap={{scale: 0.9}}
+                        transition={{ type: "spring", stiffness: 400, damping: 17, duration: 0.25 }}
+                    >Start Practicing</motion.button>
                 </div>
             </div>
         </div>
